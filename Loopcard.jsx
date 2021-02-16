e33a3f56ef74e8cb0b77a26959d2180771d0784c
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-02-16 13:06:00 +0530

    Components

---
+++ Loopcard.jsx
@@ -0,0 +1,24 @@
+import NetflixCard from './NetflixCard';
+import Moviecollect from './Moviecollect';
+ 
+const ncard =(val,i)=>{
+   return(
+    <div className="col-md-3 col-sm-6 col-lg-3"  >
+        <NetflixCard 
+           key={i}
+           imgSrc={val.posterurl} 
+           title={val.title} movtype={val.genres} 
+           link={val.link}
+        />
+     </div> 
+   );
+}
+
+function Loopcard(){
+    // calling ncard function
+    // map method comes with key value
+   return  Moviecollect.map(ncard);  
+}
+
+export default Loopcard;
+ 
--- NetflixCard.jsx
+++ NetflixCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-  
+import   './index.css';  
+
+const cardImageba = {
+  height :'200px'
+};
 
 function NetflixCard(props){
     return(
-   
   
-    
-   <div className="card card1">
-   <img src={props.imgSrc} className="card-img-top img-fluid d-block" alt="image"/>
+  <div className="card card1 "  >
+ 
+   <img src={props.imgSrc} className="card-img-top img-fluid d-block cardImageba" style={cardImageba} alt="image"/>
     <div className="card-body">
       <h5 className="card-title">{props.title}</h5>
       <p className="card-text">{props.movtype}</p>
