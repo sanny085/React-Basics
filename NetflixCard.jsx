e69721fda71e47dab533c8008121c535e0824b1a
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-02-20 12:21:22 +0530

    Update NetflixCard.jsx

--- NetflixCard.jsx
+++ NetflixCard.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import   './index.css';  
 
-const cardImageba = {
-  height :'200px'
-};
+ 
 
 function NetflixCard(props){
     return(
   
   <div className="card card1 "  >
  
-   <img src={props.imgSrc} className="card-img-top img-fluid d-block cardImageba" style={cardImageba} alt="image"/>
+   <img src={props.imgSrc} className="card-img-top img-fluid d-block cardImageba" style={{height:"200px"}} alt="image"/>
     <div className="card-body">
       <h5 className="card-title">{props.title}</h5>
       <p className="card-text">{props.movtype}</p>
