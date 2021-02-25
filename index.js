25b9ae3664401e8463b9ee5386bc6570a437af23
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-02-25 10:54:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,19 +14,4 @@ ReactDom.render(
 );
  
 
-{/* //   var htmlData = `<div class="operation">
-//   <button  class="pBell border-0 bg-none" style="float-left" > <i class="fas fa-bell pBell1"></i></button>
  
-// <div style="float: right">
-//   <button class="edit border-0 bg-none" > <i class="fas fa-edit edit1"></i></button>
-//   <button class="delete border-0 bg-none" > <i class="fas fa-trash-alt trash1"></i></button>
-// </div>
-// </div>
-// <div class="main   "></div>
-// <textarea class=" form-control" rows="15" cols="5" name="" id=""  ></textarea>
-// `; */}
-
-
-// var section = document.createElement('div');
-// section.innerHTML = htmlData;
-// document.getElementById("root").appendChild(section); 
