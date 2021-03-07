1e37e11d2a75e83e9101e46c038a409d803e88ea
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-07 18:02:36 +0530

    Add files via upload

---
+++ basics/src/Amazon.jsx
@@ -0,0 +1,13 @@
+import {AmazLoop} from './Loopcard';
+
+const Amazon =()=> {
+   return (
+    <div className="container-fluid mt-3" > 
+        <div className="row ">
+                <AmazLoop/> 
+        </div>
+    </div>
+    );
+}
+
+export default Amazon;
