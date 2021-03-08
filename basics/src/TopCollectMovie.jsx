231512c9bdfe6537cc99382dd7245561ebc08995
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-08 17:26:20 +0530

    Add files via upload

---
+++ basics/src/TopCollectMovie.jsx
@@ -0,0 +1,13 @@
+import {Topmovie} from './Loopcard';
+
+const TopCollectMovie =()=> {
+   return (
+    <div className="container-fluid mt-3" > 
+        <div className="row  ">
+                <Topmovie/> 
+        </div>
+    </div>
+    );
+}
+
+export default TopCollectMovie;
