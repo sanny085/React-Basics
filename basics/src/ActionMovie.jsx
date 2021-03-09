c90672d18829f80721e21c26d51e85fd999c3536
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-09 15:16:46 +0530

    Add files via upload

---
+++ basics/src/ActionMovie.jsx
@@ -0,0 +1,13 @@
+import {Actmovie} from './Loopcard';
+
+const ActionMovie =()=> {
+   return (
+    <div className="container-fluid mt-3" > 
+        <div className="row  ">
+                <Actmovie/> 
+        </div>
+    </div>
+    );
+}
+
+export default ActionMovie;
