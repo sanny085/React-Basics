a084ebc40ae88845af8d189e51ef7b753cb5dc9f
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-09 15:14:21 +0530

    Add files via upload

---
+++ basics/src/ComedyMovie.jsx
@@ -0,0 +1,13 @@
+import {ComedyMov} from './Loopcard';
+
+const ComedyMovie =()=> {
+   return (
+    <div className="container-fluid mt-3" > 
+        <div className="row  ">
+                <ComedyMov/> 
+        </div>
+    </div>
+    );
+}
+
+export default ComedyMovie;
