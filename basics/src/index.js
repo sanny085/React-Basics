00a280b56da04775dbe374b80a305455641eeb93
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-16 13:55:05 +0530

    Update index.js

--- basics/src/index.js
+++ basics/src/index.js
@@ -3,12 +3,30 @@ import ReactDom from 'react-dom';
 import './index.css';
 import App from './App';
 
+ 
 ReactDom.render(
    <>
-     <App/>
+   <App/>
    <br/><br/>
    {/*Creating attribute of Component (Component Property)*/}
-   </>,document.getElementById("root")
+  
+    </>,document.getElementById("root")
 );
  
+
+{/* //   var htmlData = `<div class="operation">
+//   <button  class="pBell border-0 bg-none" style="float-left" > <i class="fas fa-bell pBell1"></i></button>
  
+// <div style="float: right">
+//   <button class="edit border-0 bg-none" > <i class="fas fa-edit edit1"></i></button>
+//   <button class="delete border-0 bg-none" > <i class="fas fa-trash-alt trash1"></i></button>
+// </div>
+// </div>
+// <div class="main   "></div>
+// <textarea class=" form-control" rows="15" cols="5" name="" id=""  ></textarea>
+// `; */}
+
+ 
+// var section = document.createElement('div');
+// section.innerHTML = htmlData;
+// document.getElementById("root").appendChild(section); 
