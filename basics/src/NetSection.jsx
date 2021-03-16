b2ae68c040448727b6b347876cf899525f0262a6
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-16 13:57:32 +0530

    Update NetSection.jsx

--- basics/src/NetSection.jsx
+++ basics/src/NetSection.jsx
@@ -1,19 +1,15 @@
 import {Loopcard} from './Loopcard';
 
-
  function NetSection()
   {    
-      // JSX Elements
-      return(
-        <div className="container-fluid mt-3" > 
+    // JSX Elements
+    return(
+    <div className="container-fluid mt-3" > 
         <div className="row ">
-    
-        <Loopcard/> 
-      
-      </div>
-      </div>
-      );
-   
- }
+           <Loopcard/> 
+        </div>
+    </div>
+    );
+  }
 
  export default NetSection;
