e9e0de3409c9b3868e1f571b511626bf8daa8888
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-03 22:28:01 +0530

    Add files via upload

---
+++ basics/src/Icon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {FaPen, FaTimes, FaRegCircle} from 'react-icons/fa';
+
+const Icon = ({name}) => {
+    return (
+        <div className='Icon'>
+            
+          <FaPen/> <FaTimes/> <FaRegCircle/>
+        </div>
+    )
+}
+
+export default Icon;
