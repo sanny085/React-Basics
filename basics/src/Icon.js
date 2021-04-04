6fbf07b3d03da2232d067c864d8acb15cf7eef01
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-04 12:14:51 +0530

    Update Icon.js

--- basics/src/Icon.js
+++ basics/src/Icon.js
@@ -3,12 +3,17 @@ import React from 'react'
 import {FaPen, FaTimes, FaRegCircle} from 'react-icons/fa';
 
 const Icon = ({name}) => {
-    return (
-        <div className='Icon'>
-            
-          <FaPen/> <FaTimes/> <FaRegCircle/>
-        </div>
-    )
-}
+    
+    switch(name){
+        case 'circle':
+            return (<FaRegCircle className="icons" />)
+       
+        case 'cross':
+                return (<FaTimes className="icons" />)
+        default:
+            return(<FaPen className="icons" />)
+    }
+   
+};
 
 export default Icon;
