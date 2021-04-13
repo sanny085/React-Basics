40ad4a1964a6d1c194baaebd17f14936575f1422
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-13 14:30:31 +0530

    Update TicTac.jsx

--- basics/src/TicTac.jsx
+++ basics/src/TicTac.jsx
@@ -9,12 +9,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './TicTac.css';
 import Icon from './Icon';
 
+const itemArray = new Array(9).fill("");
+
 const TicTac = () => {
     const [isCross, setIsCross] = useState(false);
-    const [winMessage, setWinMessage] = useState("");
-
-    const itemArray = new Array(9).fill("s");
-
+    const [winMessage, setWinMessage] = useState(" ");
+ 
     const reloadGame = () => {
       setIsCross(false);
       setWinMessage("");
@@ -22,55 +22,93 @@ const TicTac = () => {
     };
 
     const checkIsWinner = () => {
-     
+      if(itemArray[0] === itemArray[1] && itemArray[0] === itemArray[2] && itemArray[0] !== "empty")
+        { 
+          setWinMessage(`${itemArray[0]} Won`);
+        }
+      else if(itemArray[3] === itemArray[4] && itemArray[4] === itemArray[5] && itemArray[3] !== "empty")
+        { 
+          setWinMessage(`${itemArray[3]} Won`);
+        }
+      else if(itemArray[6] === itemArray[7] && itemArray[7] === itemArray[8] && itemArray[6] !== "empty")
+        { 
+          setWinMessage(`${itemArray[6]} Won`);
+        }
+      else if(itemArray[0] === itemArray[3] && itemArray[3] === itemArray[6] && itemArray[0] !== "empty")
+        { 
+          setWinMessage(`${itemArray[0]} Won`);
+        }
+      else if(itemArray[1] === itemArray[4] && itemArray[4] === itemArray[7] && itemArray[1] !== "empty")
+        { 
+          setWinMessage(`${itemArray[1]} Won`);
+        }
+      else if(itemArray[2] === itemArray[5] && itemArray[5] === itemArray[8] && itemArray[2] !== "empty")
+        { 
+          setWinMessage(`${itemArray[2]} Won`);
+        }
+      else if(itemArray[0] === itemArray[4] && itemArray[4] === itemArray[8] && itemArray[0] !== "empty")
+        { 
+          setWinMessage(`${itemArray[0]} Won`);
+        }
+      else if(itemArray[2] === itemArray[4] && itemArray[4] === itemArray[6] && itemArray[2] !== "empty")
+        { 
+          setWinMessage(`${itemArray[2]} Won`);
+        }
+      
+  };
 
-    };
-    const changeItem = itemNumber => {
+  const changeItem = itemNumber => {
       if(winMessage){
         return toast(winMessage, {type: 'success'});
       }
       if(itemArray[itemNumber] === "empty") {
-        itemArray[itemNumber] = isCross ? "cross" : "circle";
+        itemArray[itemNumber] = isCross ? "cross" : "circle"
+        setIsCross(!isCross)
       }
       else{
         return toast("Already filled", {type: 'error'});
       }
-
-    };
-  
-    const notify = () => toast('🦄 Wow so easy!', {
-        position: "top-left",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        } );
  
+      checkIsWinner();
+   };
+
  return (
    <>
     <Container className="p-2">
-     Tic tac
+   
         <ToastContainer position="bottom-center" />
           <Row>
             <Col md={6} className="offset-md-3">
+            { winMessage ? (
+               <div className="mb-2 mt-2">
+                 <h1 className="text-success text-uppercase text-center ">
+                   {winMessage}
+                 </h1>
+                 <Button color="success" block onClick={reloadGame}>Reload Game 1</Button>
+
+               </div>
+            ) : (
+              <h1 className="text-center text-warning">
+                {isCross ? "cross" : "circle"} Turns
+              </h1>
+            ) }
               <div className="grid">
-                {itemArray.map ((item,  index) => {
+              { itemArray.map((item,  index) => {
                   return( 
-                    <Card color="warning" className="p-2">
+                    <Card color="warning" className="p-2" onClick={ ()=> changeItem(index)}>
                         <CardBody className="box bg-info">
                             <Icon name={item} className="icon text-light" /> 
                         </CardBody>
                     </Card>
                   )
-                })}
+                })
+              }
               </div>
             </Col>
           </Row>
     </Container>
-   </>
-  )
+  </>
+)
 
 }
 export default TicTac;
