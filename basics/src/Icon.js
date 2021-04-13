cc2036d87b034a944ae0d7f7e77917377f9e9c6e
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-13 14:31:23 +0530

    Update Icon.js

--- basics/src/Icon.js
+++ basics/src/Icon.js
@@ -7,12 +7,10 @@ const Icon = ({name}) => {
     switch(name) {
         case 'circle':
             return (<FaRegCircle className="icons" />)
-       
         case 'cross':
                 return (<FaTimes className="icons" />)
         default:
             return(<FaPen className="icons" />)
     }
 };
-
 export default Icon;
